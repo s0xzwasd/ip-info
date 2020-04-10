b64a326658cd66e6058ba86dc9cbf48e0c01df9e
Daniil Maslov <s0xzwasd@protonmail.com>
2020-04-10 22:59:06 +0300

    feat: configure 2 loaders in webpack

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import react from "./react.png";
 
-const App = () => <p>Hello, World!</p>;
+const App = () => {
+  return <p>Hello!</p>;
+};
 
 ReactDOM.render(<App />, document.getElementById("app"));
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/main.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/env", "@babel/react"],
+            cacheDirectory: true,
+          },
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: "file-loader",
+        options: {
+          outputPath: "images",
+        },
+      },
+    ],
+  },
+};
