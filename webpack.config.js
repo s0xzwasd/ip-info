59fb7136876e0431dd211d258eb29ff67ff5ea12
Daniil Maslov <s0xzwasd@protonmail.com>
2020-04-12 13:16:34 +0300

    [CI-6] Configure webpack (#2)
    * feat: add minicssextractplugin
    
    * feat: configure dev-server
    
    * feat: configure dev and prod modes
    
    * refactor: move use in rules to functions

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,97 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  mode: "development",
-  entry: "./src/main.js",
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "main.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/env", "@babel/react"],
-            cacheDirectory: true,
-          },
-        },
-      },
-      {
-        test: /\.less$/i,
-        use: [
-          {
+module.exports = (env = {}) => {
+  const { mode = "development" } = env;
+  const isProduction = mode === "production";
+  const isDevelopment = mode === "development";
+
+  const getStyleLoaders = () => {
+    return [
+      isProduction
+        ? {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "/build",
+            },
+          }
+        : {
             loader: "style-loader",
           },
-          {
-            loader: "css-loader",
-            options: {
-              modules: true,
-              sourceMap: true,
-            },
-          },
-          {
-            loader: "less-loader",
-            options: {
-              sourceMap: true,
-            },
-          },
-        ],
-      },
       {
-        test: /\.(png|jpe?g|gif)$/i,
-        loader: "file-loader",
+        loader: "css-loader",
         options: {
-          outputPath: "images",
+          modules: true,
+          sourceMap: true,
         },
       },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/index.html",
-    }),
-  ],
+      {
+        loader: "less-loader",
+        options: {
+          sourceMap: true,
+        },
+      },
+    ];
+  };
+
+  const getPlugins = () => {
+    const plugins = [
+      new HtmlWebpackPlugin({
+        template: "src/index.html",
+      }),
+    ];
+
+    if (isProduction) {
+      plugins.push(
+        new MiniCssExtractPlugin({
+          filename: "[hash:10].css",
+        })
+      );
+    }
+
+    return plugins;
+  };
+
+  return {
+    mode: isProduction ? "production" : isDevelopment && "development",
+    entry: "./src/main.js",
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "main.js",
+    },
+    plugins: getPlugins(),
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/env", "@babel/react"],
+              cacheDirectory: true,
+            },
+          },
+        },
+        {
+          test: /\.less$/i,
+          use: getStyleLoaders(),
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          loader: "file-loader",
+          options: {
+            outputPath: "images",
+          },
+        },
+      ],
+    },
+    devServer: {
+      open: true,
+      compress: true,
+      port: 1337,
+      clientLogLevel: "silent",
+    },
+  };
 };
