6b76f07a847c0bb98d5b4096281358e2f9ff88d8
Daniil Maslov <s0xzwasd@protonmail.com>
2020-04-22 22:03:48 +0300

    feat: add styles to mainpage

--- src/Layouts/Mainpage.tsx
+++ src/Layouts/Mainpage.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Card from '../components/Card/Card';
+import styles from './Mainpage.less';
 
 const Mainpage = () => (
-  <>
-    <Card description="Card description" data="Card data" />
-    <Card description="Card description" data="Card data" />
-    <Card description="Card description" data="Card data" />
-  </>
+  <div className={styles.mainpage}>
+    <Card description="Current IP" data="192.168.1.1" />
+    <Card description="Country / Currency" data="Finland" />
+    <Card description="Provider" data="SE ONLINE ETC." />
+  </div>
 );
 
 export default Mainpage;
