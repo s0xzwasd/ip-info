e7c8482acfe7890e2446db5620f0bbea4db0d577
Daniil Maslov <s0xzwasd@protonmail.com>
2020-04-22 21:56:53 +0300

    feat: add styles to card component

--- src/Layouts/Mainpage.tsx
+++ src/Layouts/Mainpage.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import Card from '../components/Card/Card';
 
-const Mainpage = () => <Card description="Card description" data="Card data" />;
+const Mainpage = () => (
+  <>
+    <Card description="Card description" data="Card data" />
+    <Card description="Card description" data="Card data" />
+    <Card description="Card description" data="Card data" />
+  </>
+);
 
 export default Mainpage;
