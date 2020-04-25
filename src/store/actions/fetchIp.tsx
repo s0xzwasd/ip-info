bcb8a6d2d599fabacdfdc6ebbe9f2000260e4123
Daniil Maslov <i@dmaslov.dev>
2020-04-25 22:20:57 +0300

    [CI-0] Small refactors (#16)
    * fix: move apiUrl to constant and add custom fields to fetchIp
    
    * feat: add currency

--- src/store/actions/fetchIp.tsx
+++ src/store/actions/fetchIp.tsx
@@ -2,8 +2,11 @@ import { Dispatch } from 'react';
 import { FETCH_IP } from '../types';
 
 export default function fetchIP() {
+  const apiBase = 'http://ip-api.com/json';
+  const fields = 'status,message,country,lat,lon,currency,isp,mobile,proxy,hosting,query';
+
   return async (dispatch: Dispatch<any>) => {
-    const response = await fetch('http://ip-api.com/json');
+    const response = await fetch(`${apiBase}?fields=${fields}`);
     const json = await response.json();
 
     dispatch({ type: FETCH_IP, payload: json });
