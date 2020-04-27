75e6922824ab6427f46eea34dc5b2d3afb042979
Daniil Maslov <s0xzwasd@protonmail.com>
2020-04-27 20:46:05 +0300

    feat: add basic map implementation

--- src/components/Map/Map.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import styles from './Map.less';
-import map from './map.png';
-
-const Map = () => <img className={styles.map} src={map} alt="" />;
-
-export default Map;
